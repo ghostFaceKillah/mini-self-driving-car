9984d2aefdc3c8db0afa9d025a0fc3c3ff5a5c60
ghostFaceKillah <garmulewicz@gmail.com>
2017-07-23 14:44:08 +0000

    Test code for sending events to raspi

--- src/test_send.py
+++ src/test_send.py
@@ -1,6 +1,7 @@
 import socket
 import sys
 import time
+import keyboard
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -11,28 +12,45 @@ print >>sys.stderr, 'connecting to %s port %s' % server_address
 sock.connect(server_address)
 
 
-MESSAGES = [
-    'ux',
-    'uo',
+def send(msg):
+    print 'sending "{}"'.format(msg)
+    sock.sendall(msg)
+    pass
 
-    'lx',
-    'lo',
 
-    'rx',
-    'ro',
-]
+def turn_right():
+    send('rx')
 
+def stop_turning():
+    send('ro')
+
+
+def turn_left():
+    send('lx')
+
+
+def go_forward():
+    send('ux')
+
+
+def stop():
+    send('uo')
+    print "Stopping the car"
+    GPIO.output(Motor2E, GPIO.LOW)
+
+
+def register_hooks():
+    print "Registering hooks"
+    keyboard.hook_key('up', keydown_callback=go_forward, keyup_callback=stop)
+    keyboard.hook_key('left', keydown_callback=turn_left, keyup_callback=stop_turning)
+    keyboard.hook_key('right', keydown_callback=turn_right, keyup_callback=stop_turning)
+    print "Hooks registered"
 
 
 try:
-    print "will send test packages"
-
-    # Send data
-    for msg in MESSAGES:
-        print 'sending "{}"'.format(msg)
-        sock.sendall(msg)
-        time.sleep(0.5)
+    register_hooks()
 
 finally:
     print >>sys.stderr, 'closing socket'
     sock.close()
+
