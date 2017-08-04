314a7f727418236ccd9143ee3f83cd037a918b4d
ghostFaceKillah <garmulewicz@gmail.com>
2017-08-05 01:27:08 +0200

    Remote fixes to raspi.

--- src/raspi/video.py
+++ src/raspi/video.py
@@ -6,26 +6,25 @@ import time
 import lib.constant as cnst
 
 
-def stream():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.connect((cnst.SERVER_IP, cnst.VIDEOS_STREAMING_PORT))
-    connection = client_socket.makefile('wb')
+class VideoStreaming(multiprocessing.Process):
 
-    try:
-        opts = dict(
-            resolution=cnst.VIDEO_RESOLUTION,
-            framerate=cnst.VIDEO_FRAMERATE
-        )
+    def run(self):
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        client_socket.connect((cnst.SERVER_IP, cnst.VIDEOS_STREAMING_PORT))
+        connection = client_socket.makefile('wb')
 
-        with picamera.PiCamera(**opts) as camera:
-            time.sleep(2)
-            camera.start_recording(connection, format='mjpeg')
-            camera.wait_recording(60000)
-            camera.stop_recording()
+        try:
+            opts = dict(
+                resolution=cnst.VIDEO_RESOLUTION,
+                framerate=cnst.VIDEO_FRAMERATE
+            )
 
-    finally:
-        client_socket.close()
+            with picamera.PiCamera(**opts) as camera:
+                time.sleep(2)
+                camera.start_recording(connection, format='mjpeg')
+                camera.wait_recording(60000)
+                camera.stop_recording()
 
+        finally:
+            client_socket.close()
 
-
-VideoStreaming = multiprocessing.Process(target=stream)
